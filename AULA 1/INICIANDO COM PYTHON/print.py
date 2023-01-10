22c5e0d7e06169f4adc4f71716fdb3038a03413f
Walisonmoraes <120772441+Walisonmoraes@users.noreply.github.com>
2023-01-10 02:51:07 -0400

    k

---
+++ AULA 1/INICIANDO COM PYTHON/print.py
@@ -0,0 +1,11 @@
+#Temos uma variedade de formas de imprimir texto e variável em Python. Vejamos algumas: podemos usar formatadores de caracteres (igual em C), podemos usar a função format() e podemos criar uma string formatada.
+#  Vejamos cada uma delas:
+
+#Modo 1 - usando formatadores de caracteres (igual na linguagem C) para imprimir variável e texto
+#print("Olá %s, bem vindo a disciplina de programação. Parabéns pelo seu primeiro hello world" % (nome))
+
+# Modo 2 - usando a função format() para imprimir variável e texto
+#print("Olá {}, bem vindo a disciplina de programação. Parabéns pelo seu primeiro hello world".format(nome))
+
+ #Modo 3 - usando strings formatadas
+print(f"Olá {nome}, bem vindo a disciplina de programação. Parabéns pelo seu primeiro hello world")
